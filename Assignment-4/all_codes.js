fd0a4962df55a24d5518266d307f1ebf7ff61834
nusrat-jahan20 <nusrat.jahan.tasnim20@gmail.com>
2023-02-07 23:00:45 +0600

    update 2nd prblm

--- Assignment-4/all_codes.js
+++ Assignment-4/all_codes.js
@@ -1,5 +1,5 @@
 // Problem 1: Let’s play a mind game
-// 'mindGame' function will take a positive number as input. Then it will multiply the number by 3, then adding 10 to the result, then dividing the result by 2, then subtract 5 from the result. After this whole calculation the function will return the result.
+// The 'mindGame' function will take a positive number as input. Then it will multiply the number by 3, then adding 10 to the result, then dividing the result by 2, then subtract 5 from the result. After this whole calculation the function will return the result.
 
 function mindGame(positiveNumber) {
     if(typeof(positiveNumber) == 'number') {
@@ -8,5 +8,29 @@ function mindGame(positiveNumber) {
     }
     else {
         return 'Please input a positive number.'
+    }
+}
+
+
+
+
+// Problem 2: Finding even or odd
+// The 'evenOdd' function will take a string as input. The function will return 'even' if the input string length is even otherwise if the length is odd then the function will return 'odd'.
+
+function evenOdd(string) {
+    if(typeof(string) == 'string') {
+        sum = 0;
+        for(i = 0; i < string.length; i++) {
+            sum++;
+        }
+        if(sum % 2 == 0) {
+            return 'even';
+        }
+        else {
+            return 'odd';
+        }
+    }
+    else {
+        return 'Please input string.'
     }
 }
--- Assignment-4/finding_even_or_odd.js
+++ Assignment-4/finding_even_or_odd.js
@@ -1,18 +1,23 @@
 // Problem 2: Finding even or odd
-// 
+// The 'evenOdd' function will take a string as input. The function will return 'even' if the input string length is even otherwise if the length is odd then the function will return 'odd'.
 
 function evenOdd(string) {
-    sum = 0;
-    for(i = 0; i < string.length; i++) {
-        sum++;
-    }
-    if(sum % 2 == 0) {
-        return 'even';
+    if(typeof(string) == 'string') {
+        sum = 0;
+        for(i = 0; i < string.length; i++) {
+            sum++;
+        }
+        if(sum % 2 == 0) {
+            return 'even';
+        }
+        else {
+            return 'odd';
+        }
     }
     else {
-        return 'odd';
+        return 'Please input string.'
     }
 }
 
-let output = evenOdd('Batch7');
+let output = evenOdd(true);
 console.log(output);
