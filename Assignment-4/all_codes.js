aed63392758c18a921b5445b4294cf8965e79804
nusrat-jahan20 <nusrat.jahan.tasnim20@gmail.com>
2023-02-07 23:13:22 +0600

    update prblm-3

--- Assignment-4/all_codes.js
+++ Assignment-4/all_codes.js
@@ -34,3 +34,24 @@ function evenOdd(string) {
         return 'Please input string.'
     }
 }
+
+
+
+
+// Problem 3: Is Less or Greater than seven
+// The 'isLGSeven()' function will take a number as input. The function have to find the difference between the number and 7, then it will compare if the difference is either less than 7 or not. If the difference is less then 7 then the function will return the difference. If not then the function will return double of the input number.
+
+function isLGSeven(number) {
+    if (typeof(number) == 'number') {
+        const difference = number - 7;
+        if(difference < 7) {
+            return difference;
+        }
+        else {
+            return (number * 2);
+        }
+    }
+    else {
+        return 'Please input a valid number.'
+    }
+}
--- Assignment-4/is_less_or_greater_than_seven.js
+++ Assignment-4/is_less_or_greater_than_seven.js
@@ -1,5 +1,5 @@
 // Problem 3: Is Less or Greater than seven
-// The 'isLGSeven()'
+// The 'isLGSeven()' function will take a number as input. The function have to find the difference between the number and 7, then it will compare if the difference is either less than 7 or not. If the difference is less then 7 then the function will return the difference. If not then the function will return double of the input number.
 
 function isLGSeven(number) {
     const difference = number - 7;
@@ -11,5 +11,5 @@ function isLGSeven(number) {
     }
 }
 
-let output = isLGSeven(15);
+let output = isLGSeven(14);
 console.log(output);
